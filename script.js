5ff8bfce6fec86e386ffd3fdc83e96793e8604c1
Harika-t634 <harikathunu.53@gmail.com>
2026-06-03 23:15:53 +0530

    Started journey of learning JavaScript and make some simple buttons using javascript

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-// console.log("Hello World");
-// // alert("This is Harika and says you welcome to my website");
-// // window.alert("Hello world");
-// // let har=document.getELementById="new_di";
-// // console.dir(har);
-// // let heads=document.getElementsByClassName("headinsd");
-// // console.log(heads);
-// // console.dir(heads);
+// These are practise codes
+console.log("Hello World");
+alert("This is Harika and says you welcome to my website");
+window.alert("Hello world");
+let har=document.getELementById="new_di";
+console.dir(har);
+let heads=document.getElementsByClassName("headinsd");
+console.log(heads);
+console.dir(heads);
 
-// let divi=document.querySelector("div");
-// console.log(divi);
+let divi=document.querySelector("div");
+console.log(divi);
 
-// let attri=divi.getAttribute("id");
-// console.log(attri);
+let attri=divi.getAttribute("id");
+console.log(attri);
 
-// let newat=divi.setAttribute("class","newClass");
-// console.log(newat);
+let newat=divi.setAttribute("class","newClass");
+console.log(newat);
 
-// divi.innerHTML="i am harika.T";
+divi.innerHTML="i am harika.T";
 
-// let btn1=document.querySelector("#btn1");
-// let currMode="light";
-// let body=document.querySelector("body");
-// btn1.addEventListener("click",(evt)=>{
-//     if(currMode==="light"){
-//         console.log("light-mode");
-//         currMode="dark";
-//         body.classList.remove("light");
-//         body.classList.add("dark");
-//     }
-//     else{
-//         currMode="light";
-//         body.classList.remove("dark");
-//         body.classList.add("light");
-//     }
+// This is toggle button code
 
-// });
+let btn1=document.querySelector("#btn1");
+let currMode="light";
+let body=document.querySelector("body");
+btn1.addEventListener("click",(evt)=>{
+    if(currMode==="light"){
+        console.log("light-mode");
+        currMode="dark";
+        body.classList.remove("light");
+        body.classList.add("dark");
+    }
+    else{
+        currMode="light";
+        body.classList.remove("dark");
+        body.classList.add("light");
+    }
+
+});
+
+// this is a mouseover event code (Did on own)
 
 let divi=document.querySelector("#divi");
 let body=document.querySelector("body");
